7d6512cf1f6e39f04a8924b377af8b6068cec504
Kothalawalage Eesha Thanukshi Wickramasinghe <thanukshi1234@gmail.com>
2022-05-17 12:31:41 +0530

    IT18202632

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const routes = require("express").Router();
 
 const UserRoutes = require("./UserRoutes");
 
+routes.get("/", (req, res) => {
+  res.send("This is User Management System");
+});
+
 routes.use("/user", UserRoutes);
 //routes.use("/loan", LoanRoutes);
 
